79bb56d1540282ae915d6d3ea370d0458c226631
Zachiel <ezehiel182@gmail.com>
2023-02-23 19:36:57 +0100

    Fixed queries, fixed html template

--- queries.py
+++ queries.py
@@ -27,9 +27,6 @@ def get_boards():
     Gather all boards
     :return:
     """
-    # remove this code once you implement the database
-    return [{"title": "board1", "id": 1}, {"title": "board2", "id": 2}]
-
     return data_manager.execute_select(
         """
         SELECT * FROM boards
@@ -39,8 +36,6 @@ def get_boards():
 
 
 def get_cards_for_board(board_id):
-    # remove this code once you implement the database
-    return [{"title": "title1", "id": 1}, {"title": "board2", "id": 2}]
 
     matching_cards = data_manager.execute_select(
         """
