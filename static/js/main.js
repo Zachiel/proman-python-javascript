b4f2f40cce922ad69e1312f8162de3efd621817d
Mateusz Piszczatowski <mateusz.piszczatowski@gmail.com>
2023-02-24 12:46:40 +0100

    Add header positioning and Boards accordion

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,10 @@
 import {boardsManager} from "./controller/boardsManager.js";
 
 function init() {
-    boardsManager.loadBoards();
+    //boardsManager.loadBoards();
+    document.querySelector('.board__add-card-button').addEventListener('click', ()=>{alert('test')});
+    console.log('ready');
 }
+window.onload=()=>{init()};
 
-init();
+
