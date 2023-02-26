0f4dd3a8daf42ce4d876e0cc3e1c3e5a5ebfce39
Zachiel <ezehiel182@gmail.com>
2023-02-26 22:47:01 +0100

    Added methods

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 "Python server file containing routes and responses."
 # pylint: disable=unused-import
+### TODO: add method description to docstrings
 from typing import Any
 import uuid
 import re
 import mimetypes
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, flash, get_flashed_messages, render_template, url_for, request, redirect
 from flask import session, abort
 from flask.typing import ResponseReturnValue
 from dotenv import load_dotenv
@@ -20,9 +21,14 @@ app.config['MAX_CONTENT_LENGTH'] = 1 * 1000 * 1000
 load_dotenv()
 
 
-@app.route("/")
+@app.route("/",
+    methods="GET")
 def index() -> str:
     """Root route which displays all boards and cards.
+
+    Methods
+    -------
+    get
 
     Returns
     -------
@@ -33,9 +39,15 @@ def index() -> str:
     return render_template('pages/index.html')
 
 
-@app.route("/register")
+@app.route("/register",
+    methods="POST")
 def registration() -> Any:
     """Route to register a new user to database.
+
+    Methods
+    -------
+    post
+        save new user record into database
 
     Returns
     -------
@@ -50,9 +62,15 @@ def registration() -> Any:
     data_handler.users.register_new_user(new_user)
 
 
-@app.route("/login")
+@app.route("/login",
+    methods="POST")
 def login() -> ResponseReturnValue:
     """Route for checking credentials validity
+
+    Methods
+    -------
+    post
+        safe payload exchange for input values
 
     Returns
     -------
@@ -69,9 +87,14 @@ def login() -> ResponseReturnValue:
     return abort(401)
 
 
-@app.route("/logout")
+@app.route("/logout",
+    methods="GET")
 def logout() -> ResponseReturnValue:
     """Route for logging out an user and clearing session data.
+
+    Methods
+    -------
+    get
 
     Returns
     -------
@@ -86,24 +109,44 @@ def logout() -> ResponseReturnValue:
 
 
 
-@app.route("/api/boards")
+@app.route("/api/boards",
+    methods=["GET", "POST"])
 @json_response
-def get_all_public_boards() -> ResponseReturnValue:
-    """Get all boards from the database.
+def public_boards() -> ResponseReturnValue | None:
+    """Route for retrieving or creating public boards.
+
+    Methods
+    -------
+    get
+        retrieve all public boards
+    post
+        save a new record into boards database without owner info
 
     Returns
     -------
     ResponseReturnValue : dict[str, ...]
         JSON object
+    None
+        flashed message
     """
 
-    return data_handler.boards.get_all_public_boards()
+    if request.method == "POST":
+        data: Any = request.json
+        data_handler.boards.post_public_board(data["title"])
+        flash(f"board {data['title']} created succesfuly!", "message")
+    else:
+        return data_handler.boards.get_all_public_boards()
 
 
-@app.route("/api/boards/<int:board_id>")
+@app.route("/api/boards/<int:board_id>",
+    methods=["GET", "PUT", "PATCH", "DELETE"])
 @json_response
-def get_public_board(board_id: int) -> ResponseReturnValue:
+def public_board(board_id: int) -> ResponseReturnValue | None:
     """Get specified board from the database.
+
+    Methods
+    -------
+    get, put, patch, delete
 
     Parameters
     ----------
@@ -114,15 +157,22 @@ def get_public_board(board_id: int) -> ResponseReturnValue:
     -------
     ResponseReturnValue : dict[str, ...]
         JSON object
+    None
+        flashed message
     """
 
     return data_handler.boards.get_public_board(board_id)
 
 
-@app.route("/api/users/<int:user_id>/boards")
+@app.route("/api/users/<int:user_id>/boards",
+    methods=["GET", "POST"])
 @json_response
-def get_all_user_public_boards(user_id: int) -> ResponseReturnValue:
+def user_public_boards(user_id: int) -> ResponseReturnValue | None:
     """Get all boards owned by specified user from the database.
+
+    Methods
+    -------
+    get, post
 
     Parameters
     ----------
@@ -133,15 +183,22 @@ def get_all_user_public_boards(user_id: int) -> ResponseReturnValue:
     -------
     ResponseReturnValue : dict[str, ...]
         JSON object
+    None
+        flashed message
     """
 
     return data_handler.boards.get_all_user_public_boards(user_id)
 
 
-@app.route("/api/users/<int:user_id>/boards/<int:board_id>")
+@app.route("/api/users/<int:user_id>/boards/<int:board_id>",
+    methods=["GET", "PUT", "PATCH", "DELETE"])
 @json_response
-def get_user_public_board(user_id: int, board_id: int) -> ResponseReturnValue:
+def user_public_board(user_id: int, board_id: int) -> ResponseReturnValue | None:
     """Get specified board owned by specified user from the database.
+
+    Methods
+    -------
+    get, put, patch, delete
 
     Parameters
     ----------
@@ -154,15 +211,22 @@ def get_user_public_board(user_id: int, board_id: int) -> ResponseReturnValue:
     -------
     ResponseReturnValue : dict[str, ...]
         JSON object
+    None
+        flashed message
     """
 
     return data_handler.boards.get_user_public_board(user_id, board_id)
 
 
-@app.route("/api/boards/<int:board_id>/cards")
+@app.route("/api/boards/<int:board_id>/cards",
+    methods=["GET", "POST"])
 @json_response
-def get_all_cards_public_board(board_id: int) -> ResponseReturnValue:
+def cards_public_board(board_id: int) -> ResponseReturnValue | None:
     """Get all cards belonging to specified board from the database.
+
+    Methods
+    -------
+    get, post
 
     Parameters
     ----------
@@ -173,15 +237,22 @@ def get_all_cards_public_board(board_id: int) -> ResponseReturnValue:
     -------
     ResponseReturnValue : dict[str, ...]
         JSON object
+    None
+        flashed message
     """
 
     return data_handler.cards.get_all_cards_public_board(board_id)
 
 
-@app.route("/api/boards/<int:board_id>/cards/<int:card_id>")
+@app.route("/api/boards/<int:board_id>/cards/<int:card_id>",
+    methods=["GET", "PUT", "PATCH", "DELETE"])
 @json_response
-def get_card_public_board(board_id: int, card_id: int) -> ResponseReturnValue:
+def card_public_board(board_id: int, card_id: int) -> ResponseReturnValue | None:
     """Get specified card for a specified board from the database.
+
+    Methods
+    -------
+    get, put, patch, delete
 
     Parameters
     ----------
@@ -194,15 +265,23 @@ def get_card_public_board(board_id: int, card_id: int) -> ResponseReturnValue:
     -------
     ResponseReturnValue : dict[str, ...]
         JSON object
+    None
+        flashed message
     """
 
     return data_handler.cards.get_card_public_board(board_id, card_id)
 
 
-@app.route("/api/users/<int:user_id>/boards/<int:board_id>/cards")
+@app.route("/api/users/<int:user_id>/boards/<int:board_id>/cards",
+    methods=["GET", "POST"])
 @json_response
-def get_all_cards_user_public_board(user_id: int, board_id: int) -> ResponseReturnValue:
+def cards_user_public_board(
+    user_id: int, board_id: int) -> ResponseReturnValue | None:
     """Get all cards belonging to specified user board from the database.
+
+    Methods
+    -------
+    get, post
 
     Parameters
     ----------
@@ -215,15 +294,23 @@ def get_all_cards_user_public_board(user_id: int, board_id: int) -> ResponseRetu
     -------
     ResponseReturnValue : dict[str, ...]
         JSON object
+    None
+        flashed message
     """
 
     return data_handler.cards.get_all_cards_user_public_board(user_id, board_id)
 
 
-@app.route("/api/users/<int:user_id>/boards/<int:board_id>/cards/<int:card_id>")
+@app.route("/api/users/<int:user_id>/boards/<int:board_id>/cards/<int:card_id>",
+    methods=["GET", "PUT", "PATCH", "DELETE"])
 @json_response
-def get_card_user_public_board(user_id: int, board_id: int, card_id: int) -> ResponseReturnValue:
+def card_user_public_board(
+    user_id: int, board_id: int, card_id: int) -> ResponseReturnValue | None:
     """Get all cards belonging to the specified user board from the database.
+
+    Methods
+    -------
+    get, put, patch, delete
 
     Parameters
     ----------
@@ -238,29 +325,44 @@ def get_card_user_public_board(user_id: int, board_id: int, card_id: int) -> Res
     -------
     ResponseReturnValue : dict[str, ...]
         JSON object
+    None
+        flashed message
     """
 
-    return data_handler.cards.get_card_user_public_board(user_id, board_id, card_id)
+    return data_handler.cards.get_card_user_public_board(
+        user_id, board_id, card_id)
 
 
-@app.route("/api/users")
+@app.route("/api/users",
+    methods=["GET", "POST"])
 @json_response
-def get_all_users() -> ResponseReturnValue:
+def users() -> ResponseReturnValue | None:
     """Get all users from the database.
-            
+
+    Methods
+    -------
+    get, post
+
     Returns
     -------
     ResponseReturnValue : dict[str, ...]
         JSON object
+    None
+        flashed message
     """
 
     return data_handler.users.get_all_users()
 
 
-@app.route("/api/users/<int:user_id>")
+@app.route("/api/users/<int:user_id>",
+    methods=["GET", "PUT", "PATCH", "DELETE"])
 @json_response
-def get_user(user_id: int) -> ResponseReturnValue:
+def user(user_id: int) -> ResponseReturnValue | None:
     """Get specified user profile from database.
+
+    Methods
+    -------
+    get, put, patch, delete
 
     Parameters
     ----------
@@ -271,6 +373,8 @@ def get_user(user_id: int) -> ResponseReturnValue:
     -------
     ResponseReturnValue : dict[str, ...]
         JSON object
+    None
+        flashed message
     """
 
     return data_handler.users.get_user(user_id)
@@ -282,6 +386,7 @@ def get_user(user_id: int) -> ResponseReturnValue:
 def main() -> None:
     """Starts flask server listening on localhost:5000
     """
+
     app.run(debug=True, host='0.0.0.0', port=5000)
 
     # Serving the favicon
