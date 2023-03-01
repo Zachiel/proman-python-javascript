de925aa5f0b938c22824345bb8ed7983a1f49ce3
Zachiel <ezehiel182@gmail.com>
2023-03-01 22:49:27 +0100

    implemented board queries

--- data_handler/board_handler.py
+++ data_handler/board_handler.py
@@ -3,6 +3,7 @@
     Queries regarding boards.
 """
 from typing import Any
+from functools import reduce
 import data_manager
 
 
@@ -19,7 +20,7 @@ def get_all_public_boards() -> Any:
         SELECT *
         FROM boards
         WHERE is_private = FALSE
-    """
+        """
     public_boards: Any = data_manager.execute_select(query)
 
     return public_boards
@@ -42,7 +43,7 @@ def get_public_board(board_id: int) -> Any:
         FROM boards
         WHERE id = %(id)s
         AND is_private = FALSE
-    """
+        """
     public_board: Any = data_manager.execute_select(query, {"id": board_id}, False)
 
     return public_board
@@ -67,7 +68,7 @@ def get_all_user_public_boards(user_id: int) -> Any:
         LEFT JOIN user_boards AS ub ON b.id = ub.board_id
         WHERE is_private = FALSE
         AND ub.user_id = %(id)s
-    """
+        """
     public_boards: Any = data_manager.execute_select(query, {"id": user_id})
 
     return public_boards
@@ -94,7 +95,7 @@ def get_user_public_board(user_id: int, board_id: int) -> Any:
         WHERE b.is_private = FALSE
         AND ub.user_id = %(user_id)s
         AND b.id = %(board_id)s
-    """
+        """
     public_board: Any = data_manager.execute_select(query,
         {"user_id": user_id, "board_id": board_id}, False)
 
@@ -102,7 +103,15 @@ def get_user_public_board(user_id: int, board_id: int) -> Any:
 
 
 def post_public_board(title: str, owner_id: int=0) -> None:
+    """Create new public board.
 
+    Parameters
+    ----------
+    title : str
+        new board title
+    owner_id : int, optional
+        if >0 binds the board to the specified user, by default 0
+    """
 
     query_boards: str = """
         INSERT INTO boards (title)
@@ -110,15 +119,79 @@ def post_public_board(title: str, owner_id: int=0) -> None:
             %(title)s
         )
         RETURNING id
-    """
+        """
     query_user_boards: str = """
         INSERT INTO user_boards (board_id, user_id, role)
         VALUES (
             %(id)s,
             %(owner_id)s,
             'owner_id'
-        );
-    """
+        )
+        """
     board_id: Any = data_manager.execute_insert(query_boards, {"title": title}, True)
     if owner_id > 0:
         data_manager.execute_insert(query_user_boards, {"id": board_id,"owner_id": owner_id})
+
+
+def post_private_board(title: str, owner_id: int) -> None:
+    """Create new private board.
+
+    Parameters
+    ----------
+    title : str
+        new board title
+    owner_id : int
+        user to bind board to
+    """
+
+    query_boards: str = """
+        INSERT INTO boards (title, is_private)
+        VALUES (
+            %(title)s, TRUE
+        )
+        RETURNING id
+        """
+    query_user_boards: str = """
+        INSERT INTO user_boards (board_id, user_id, role)
+        VALUES (
+            %(id)s,
+            %(owner_id)s,
+            'owner_id'
+        )
+        """
+    board_id: Any = data_manager.execute_insert(query_boards,
+        {"title": title}, True)
+    data_manager.execute_insert(query_user_boards,
+        {"id": board_id,"owner_id": owner_id})
+
+
+def delete_board(board_id: int, user_id: int=0) -> None:
+
+
+    query_boards: str = """
+    DELETE FROM boards
+    WHERE board_id = %(board_id)s
+    """
+    query_user_boards: str = """
+    DELETE FROM user_boards
+    WHERE board_id = %(board_id)s
+    AND user_id = %(user_id)s
+    """
+    query_cards: str = """
+    DELETE FROM cards
+    WHERE board_id = %(board_id)s
+    """
+    data_manager.execute_other(query_cards, {"board_id": board_id})
+    data_manager.execute_other(query_user_boards,
+        {"board_id": board_id, "user_id": user_id})
+    data_manager.execute_other(query_boards, {"board_id": board_id})
+
+
+def patch_board(board_id: int, data: dict[str, Any]) -> None:
+
+
+    data_insert_str: str = "SET %s = %s" + ", %s = %s" * (len(data)-1)
+    query: str = "UPDATE boards" + data_insert_str + "WHERE id = %(board_id)s"
+    data_list: list[str | int] = list(reduce(lambda k, v: k + v, data.items()))
+    data_list.append(board_id)
+    data_manager.execute_other(query, data_list)
