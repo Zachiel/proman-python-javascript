bb2d26070e50fd07f9281d5172e4f59282e3aabb
Mateusz Piszczatowski <mateusz.piszczatowski@gmail.com>
2023-03-03 21:18:14 +0100

    Implement server-side registration

--- util.py
+++ util.py
@@ -3,6 +3,7 @@
 from functools import wraps
 from typing import Callable
 from flask import Response, jsonify
+from re import search as regex_search
 
 
 def json_response(func: Callable) -> Callable[..., Response]:
@@ -12,3 +13,22 @@ def json_response(func: Callable) -> Callable[..., Response]:
         return jsonify(func(*args, **kwargs))
 
     return decorated_function
+
+
+def regex_validate(regex: str, string: str) -> bool:
+    """Check if provided string matches provided regex
+
+    Parameters
+    ----------
+    regex : str
+        Regular expression to match the string with
+
+    string : str
+        String to be matched with regular expression
+
+    Returns
+    -------
+    bool
+        True if the string match regex formula, False otherwise
+    """
+    return not (regex_search(regex, string) is None)
